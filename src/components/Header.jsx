052a5e2afbff55c407ed7f1cd84ea9e244e62a0d
guilherme forssetto correa <guiforssetto07@gmail.com>
2024-07-30 16:49:00 -0700

    header started

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
+
 export default function Header(){
     return <header>
-            <h1>ESSE E O SITE DA AS ETIQUETAS</h1>
+            <section>
+                
+            </section>
+            <img src="/imgs/as_logo.png" alt="logo" />
     </header>
 }

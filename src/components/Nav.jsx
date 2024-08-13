93343fd0c469a5d9dba85e1ba1566f242db0aa64
guilherme forssetto correa <guiforssetto07@gmail.com>
2024-08-12 19:20:24 -0700

    tablet header css done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,8 @@ export default function Nav(){
     return <nav>
         <ul>
             <li><a href="#home">Home</a></li>
-            <li><div className="nav-bars"></div></li> 
             <li><a href="#sobre">Sobre Nos</a></li>
-            <li><div className="nav-bars"></div></li> 
             <li><a href="#produtos">Nossos produtos</a></li>
-            <li><div className="nav-bars"></div></li> 
             <li><a href="#contato">Contato</a></li>
         </ul>
     </nav>

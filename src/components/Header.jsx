0d4a542ecaa20a2cf90501a81ea61ed2d5db56a0
guilherme forssetto correa <guiforssetto07@gmail.com>
2025-01-12 20:22:56 -0800

    changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 
-import Nav from './Nav';
+import Nav from './NavMobile';
 
 export default function Header(){
     return (
         <header className="header">
-            <section className="header-section">
-                <Nav />
+            <Nav/>
+            <section>
             </section>
-            <img src="/imgs/as_logo_nb.png" alt="Company Logo" className="header-logo" />
         </header>
     );
 }

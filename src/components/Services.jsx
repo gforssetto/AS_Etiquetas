fde4eeeddd805fb9d91348f3350682e9b6524f8d
guilherme forssetto correa <guiforssetto07@gmail.com>
2025-05-27 17:07:04 -0700

    fix: remove homepage for Vercel build

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,17 +4,17 @@ export default function Services() {
             <h2>Nossos Produtos</h2>
             <div className="services-container">
                 <div className="service-card">
-                    <img src="imgs\img_etq_1.jpeg" alt="Serviço 1" />
+                    <img src="/imgs/img_etq_1.jpeg" alt="Serviço 1" />
                     <h3>Etiquetas de Balança Térmica</h3>
                     <p>Ideais para supermercados e padarias, garantem impressões nítidas e duradouras.</p>
                 </div>
                 <div className="service-card">
-                    <img src="imgs\img_etq_2.jpeg" alt="Serviço 2" />
+                    <img src="/imgs/img_etq_2.jpeg" alt="Serviço 2" />
                     <h3>Etiquetas Frágil para Entrega</h3>
                     <p>Sinaliza pacotes delicados, garantindo cuidado no transporte.</p>
                 </div>
                 <div className="service-card">
-                    <img src="imgs\img_etq_3.jpeg" alt="Serviço 3" />
+                    <img src="/imgs/img_etq_3.jpeg" alt="Serviço 3" />
                     <h3>Ribbons para Impressão</h3>
                     <p>Alta qualidade para impressoras térmicas, oferecendo durabilidade e acabamento profissional.</p>
                 </div>

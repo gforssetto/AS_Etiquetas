48c316cca4d9fd7e9dd80b4b2983e87581531b8f
guilherme forssetto correa <guiforssetto07@gmail.com>
2025-05-28 16:20:24 -0700

    style: ajustes finais no CSS agr ss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
     return (
         <footer className="footer">
-            <p>&copy; {new Date().getFullYear()} Sua Empresa. Todos os direitos reservados.</p>
-            <ul>
+            <p>&copy; {new Date().getFullYear()} AS etiquetas. Todos os direitos reservados.</p>
+            {/* <ul>
                 <li><a href="#">Política de Privacidade</a></li>
                 <li><a href="#">Termos de Serviço</a></li>
                 <li><a href="#">Contato</a></li>
-            </ul>
+            </ul> */}
         </footer>
     );
 }
